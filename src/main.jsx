42b6473793851f33cc361c5cb7607124a97da7f8
Kanon dev <kanon2017sengrem@gmail.com>
2024-11-18 13:59:09 +0600

    basic react router dom setup done

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main/Main.jsx';
+import Home from './Components/Home/Home.jsx';
+import Login from './Components/Login/Login.jsx';
+import SignUp from './Components/SignUp/SignUp.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path:'login',
+        element:<Login></Login>
+      },
+      {
+        path:'signup',
+        element:<SignUp></SignUp>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
